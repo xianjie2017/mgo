a96ba12a89c079e80cefdb96cf3d99be88b091af
xj <42540403@qq.com>
2021-01-28 13:59:12 +0800

    增加array, string_array, numeric_array三种字段类型

--- src/MongoManager.php
+++ src/MongoManager.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace Kckj\Mgo;
 
 use Doctrine\ODM\MongoDB\Configuration;
@@ -17,7 +18,11 @@ use Doctrine\ODM\MongoDB\Hydrator\HydratorFactory;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 use Doctrine\ODM\MongoDB\Persisters\DocumentPersister;
 use Hyperf\Contract\ConfigInterface;
+use Kckj\Mgo\Type\Arr;
+use Kckj\Mgo\Type\NumericArray;
+use Kckj\Mgo\Type\StringArray;
 use MongoDB\Client;
+use Doctrine\ODM\MongoDB\Types\Type;
 
 class MongoManager
 {
@@ -26,7 +31,7 @@ class MongoManager
 
     public function __construct(ConfigInterface $config)
     {
-        if (! $this->client instanceof Client) {
+        if (!$this->client instanceof Client) {
             $this->client = new Client($config->get('mongodb.uri', 'mongodb://127.0.0.1:27017'), [
                 'maxPoolSize' => $config->get('mongodb.maxPoolSize', 50),
                 'minPoolSize' => $config->get('mongodb.minPoolSize', 5),
@@ -35,17 +40,44 @@ class MongoManager
                 'waitQueueTimeoutMS' => $config->get('mongodb.waitQueueTimeoutMS', 1000),
             ], ['typeMap' => DocumentManager::CLIENT_TYPEMAP]);
         }
+
+        Type::addType('string_array', StringArray::class);
+        Type::overrideType('string_array', StringArray::class);
+        Type::registerType('string_array', StringArray::class);
+
+        Type::addType('array', Arr::class);
+        Type::overrideType('array', Arr::class);
+        Type::registerType('array', Arr::class);
+
+        Type::addType('numeric_array', NumericArray::class);
+        Type::overrideType('numeric_array', NumericArray::class);
+        Type::registerType('numeric_array', NumericArray::class);
+    }
+
+    /**
+     * 重链
+     * @param ConfigInterface $config
+     */
+    public function ReConnect(ConfigInterface $config)
+    {
+        $this->client = new Client($config->get('mongodb.uri', 'mongodb://127.0.0.1:27017'), [
+            'maxPoolSize' => $config->get('mongodb.maxPoolSize', 50),
+            'minPoolSize' => $config->get('mongodb.minPoolSize', 5),
+            'maxIdleTimeMS' => $config->get('mongodb.maxIdleTimeMS', 5 * 60 * 1000),
+            'waitQueueMultiple' => $config->get('mongodb.maxIdleTimeMS', 10),
+            'waitQueueTimeoutMS' => $config->get('mongodb.waitQueueTimeoutMS', 1000),
+        ], ['typeMap' => DocumentManager::CLIENT_TYPEMAP]);
     }
 
     public function DocumentManager(): DocumentManager
     {
-        if (! is_dir(BASE_PATH . '/runtime/Proxies')) {
+        if (!is_dir(BASE_PATH . '/runtime/Proxies')) {
             mkdir(BASE_PATH . '/runtime/Proxies', 0777, true);
         }
-        if (! is_dir(BASE_PATH . '/runtime/Hydrators')) {
+        if (!is_dir(BASE_PATH . '/runtime/Hydrators')) {
             mkdir(BASE_PATH . '/runtime/Hydrators', 0777, true);
         }
-        if (! is_dir(BASE_PATH . '/runtime/Documents')) {
+        if (!is_dir(BASE_PATH . '/runtime/Documents')) {
             mkdir(BASE_PATH . '/app/Mongo', 0777, true);
         }
         $config = new Configuration();
